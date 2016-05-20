28251cf1905f4aa6b8661e7cd91ee4efb9c5c858
sp0gg <joe.nelson@runbox.com>
2016-05-19 20:07:28 -0400

    -add script to download rubymine latest version

---
+++ download_latest_rubymine.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+download_location="/opt/jetbrains/"
+cd download_location
+version=`curl https://confluence.jetbrains.com/display/RUBYDEV/Previous+RubyMine+Releases | grep latest\ RubyMine | sed "s/.*RubyMine \(.*\) release.*/\1/g"`
+echo $version
+filename="RubyMine-$version.tar.gz"
+file_link="https://download.jetbrains.com/ruby/$filename"
+echo $file_link
+#wget $file_link
+chown sp0gg:sp0gg $filename
+tar -zxvf $filename
+chown -R sp0gg:sp0gg RubyMine-$version/
+cd -
