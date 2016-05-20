b0689b1c57f5e4b2a4cb182cc7e0c4a7ff89a18c
sp0gg <joe.nelson@runbox.com>
2016-05-19 20:09:57 -0400

    remove comment

--- download_latest_rubymine.sh
+++ download_latest_rubymine.sh
@@ -6,7 +6,7 @@ echo $version
 filename="RubyMine-$version.tar.gz"
 file_link="https://download.jetbrains.com/ruby/$filename"
 echo $file_link
-#wget $file_link
+wget $file_link
 chown sp0gg:sp0gg $filename
 tar -zxvf $filename
 chown -R sp0gg:sp0gg RubyMine-$version/
