c2f85e0a4d79d52e62c6d3f958303e88e5be63b9
sp0gg <joe.nelson@runbox.com>
2016-05-20 06:55:11 -0400

    get user from shell

--- download_latest_rubymine.sh
+++ download_latest_rubymine.sh
@@ -7,7 +7,7 @@ filename="RubyMine-$version.tar.gz"
 file_link="https://download.jetbrains.com/ruby/$filename"
 echo $file_link
 wget $file_link
-chown sp0gg:sp0gg $filename
+chown $USER:$USER $filename
 tar -zxvf $filename
-chown -R sp0gg:sp0gg RubyMine-$version/
+chown -R $USER:$USER RubyMine-$version/
 cd -
